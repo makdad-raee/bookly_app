1bff54df9087a33480ac39064812a6f6db75bac9
makdad-raee <makdadraeeeeee@gmail.com>
2023-12-02 10:36:50 -0800

    create app textsize

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,4 @@ const kPrimaryColor=  Color.fromRGBO(59, 26, 158, 0);
 const kPathLogo='assets/images/Logo.png';
 const kTestImage='assets/images/test_image.png';
 const kTranstionDuration=Duration(microseconds: 250);
+const kGtSectraFine='GT Sectra Fine';
