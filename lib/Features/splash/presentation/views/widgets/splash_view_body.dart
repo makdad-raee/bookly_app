9f1f67ee8277f0172c5f074a7d4ce572a98095cd
makdad-raee <makdadraeeeeee@gmail.com>
2023-12-02 00:59:07 -0800

    refactor initstate,navigate to HomePage

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_app/Features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/Features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -9,33 +12,54 @@ class SplashViewBody extends StatefulWidget {
   State<SplashViewBody> createState() => _SplashViewBodyState();
 }
 
-class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
-  late AnimationController animationController ;
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
   late Animation<Offset> slidingAnimation;
   @override
   void initState() {
-   
     super.initState();
-    animationController=AnimationController(vsync: this,duration:const Duration(seconds: 1));
-    slidingAnimation=Tween<Offset>(begin:const Offset(0, 2) ,end:Offset.zero ).animate(animationController);
+    initSlidingAnimation();
     animationController.forward();
+    navigateToHome();
   }
+
+  
+
   @override
   void dispose() {
     super.dispose();
     animationController.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(kPathLogo),
-        const SizedBox(height: 4,),
+        const SizedBox(
+          height: 4,
+        ),
         SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
   }
-}
 
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+  }
+  void navigateToHome() {
+     Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        Get.to(()=>const HomeView(),transition: Transition.fade,duration: kTranstionDuration);
+      },
+    );
+  }
+}
--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor=  Color.fromRGBO(46, 24, 112, 0);
 const kPathLogo='assets/images/Logo.png';
+const kTranstionDuration=Duration(microseconds: 250);
