c0fc4052bb4438c69633003f9741df9c739d0b40
makdad-raee <makdadraeeeeee@gmail.com>
2023-12-02 05:28:28 -0800

    some refactor to make the code clean

---
+++ lib/Features/home/presentation/views/widgets/features_books_list_view.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/featured_view_item.dart';
+import 'package:flutter/material.dart';
+
+class FeaturesBooksListView extends StatelessWidget {
+  const FeaturesBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+       height: MediaQuery.of(context).size.height * 0.25,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) =>const Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 8),
+          child: FeaturedListViewItem(),
+        ),
+      ),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_appbar.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/custom_view_item.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/featured_view_item.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/features_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -14,23 +15,6 @@ class HomeViewBody extends StatelessWidget {
         FeaturesBooksListView()
 
       ],
-    );
-  }
-}
-class FeaturesBooksListView extends StatelessWidget {
-  const FeaturesBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  SizedBox(
-       height: MediaQuery.of(context).size.height * 0.25,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) =>const Padding(
-          padding:  EdgeInsets.symmetric(horizontal: 8),
-          child: FeaturedListViewItem(),
-        ),
-      ),
     );
   }
 }
