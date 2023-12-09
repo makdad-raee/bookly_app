e9246bc83e324f8385ea31cc809218bed0efff21
makdad-raee <makdadraeeeeee@gmail.com>
2023-12-09 09:30:16 -0800

    use  setupgetIt ( ) im main dart file

--- lib/core/utils/services_locater.dart
+++ lib/core/utils/services_locater.dart
@@ -4,7 +4,7 @@ import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
-void setup(){
+void setupGetIt(){
   getIt.registerSingleton<ApiService>(ApiService(dio: Dio()));
   getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(apiService: getIt.get<ApiService>()));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,14 @@ import 'package:bookly_app/Features/home/data/repos/home_repo_imol.dart';
 import 'package:bookly_app/Features/home/presentation/manger/featured_cubit/featured_cubit.dart';
 import 'package:bookly_app/Features/home/presentation/manger/news_book_cubit/news_book_cubit.dart';
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/services_locater.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupGetIt();
   runApp(const BooklyApp());
 }
 
