81f7cf59d39a895cf70196c5fbb187f6c6ce8f5e
makdad-raee <makdadraeeeeee@gmail.com>
2023-12-09 08:03:19 -0800

    news cubit done

--- lib/Features/home/presentation/manger/news_book_cubit/news_book_cubit.dart
+++ lib/Features/home/presentation/manger/news_book_cubit/news_book_cubit.dart
@@ -1,7 +1,20 @@
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/Features/home/presentation/manger/news_book_cubit/news_book_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NewsBooksCubit extends Cubit<NewsBooksStata>{
-  NewsBooksCubit():super(NewsBooksInitialStata());
+  NewsBooksCubit(this.homeRepo):super(NewsBooksInitialStata());
+  final HomeRepo homeRepo;
   
+  
+    Future<void> fetchFeautredBooks () async {
+    emit(NewsBooksLoadingStata());
+    var result=await homeRepo.fetchNewsBooks();
+     result.fold((failure) {
+      emit(NewsBooksErrorStata(errorMessage: failure.errorMessage));
+     }, (books) {
+       emit(NewsBooksSuccessStata(books: books));
+       
+     });
+  }
 }
